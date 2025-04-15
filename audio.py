d337ae4b38b7f8ddfca0beca96b2c05144f6746d
GRAHAM EISELE 347585 <graham.w.eisele@gmail.com>
2025-04-15 20:52:12 +0200

    feat: added add gain compression method

--- audio.py
+++ audio.py
@@ -1,5 +1,10 @@
 import numpy as np
 import scipy.io.wavfile as wav
+import math
+
+
+# Purpose
+# reads in an audio file given an input file path
 
 # Params
 # path: file path of the input audio
@@ -14,8 +19,36 @@ def get_samples_and_sample_rate(path):
     
     try: 
         sample_rate, samples = wav.read(path)
+        samples = samples.astype(np.float32) 
     except:
-        print("Error reading file")
-    
+        print("Error reading file")    
 
     return [samples, sample_rate]
+
+# Purpose
+# adds gain compression to a given audio 
+
+# Params
+# samples: the array of samples of a given audio file
+# sample_rate: the sample rate of the given audio file
+# alpha: an adjustable parameter to change how much to increase distortion 
+
+def add_gain_compression(samples, alpha=1):
+
+    samples_min = np.min(samples)
+
+    for i, sample in enumerate(samples):
+        delta = sample - samples_min
+        try:
+            if delta > 0:
+                samples[i] = (1 + (alpha / math.log10(delta)))
+            else:
+                samples[i] = 1  # Or some default value to avoid distortion
+        except Exception as error:
+            print("current sample:", sample)
+            print("sample min:", samples_min)
+            print("delta:", delta)
+            print("An exception occurred:", error)
+    
+    return samples
+
--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import argparse
 import audio
 #main program file
 
-
+# parse arguments given by the user
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", help = "input filename")
     args =  parser.parse_args()
     return args
 
+# main function
 def main(args):
-    audio.get_samples_and_sample_rate(args.input)
+    samples, sample_rate = audio.get_samples_and_sample_rate(args.input)
+    samples, audio.add_gain_compression(samples, sample_rate)
 
+# to tell that it is main program
 if __name__ == "__main__":
     args = parse_arguments()
     main(args)
