ef3ba322beed7186c7fca124876a1f6900a55e26
GRAHAM EISELE 347585 <graham.w.eisele@gmail.com>
2025-04-15 18:54:22 +0200

    feat: add argparse + argument of input for input audio file

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import argparse
 #main program file
 
-def main():
-    print("test")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", help = "input filename")
+    args =  parser.parse_args()
+    return args
+
+def main(args):
+    print(args.input)
 
 if __name__ == "__main__":
-    main()
+    args = parse_arguments()
+    main(args)
