855cb16f6f41718aa09a5f7c1246f82a37b2c6c2
GRAHAM EISELE 347585 <graham.w.eisele@gmail.com>
2025-04-15 19:34:15 +0200

    feat: add function to read in audio file with given input path.

--- audio.py
+++ audio.py
@@ -1,5 +1,21 @@
 import numpy as np
 import scipy.io.wavfile as wav
 
-def hello_world():
-    print("hello world")
+# Params
+# path: file path of the input audio
+
+# Returns
+# the samples and the sample rate of the file of the given path
+
+def get_samples_and_sample_rate(path):
+
+    sample_rate = -1
+    samples = -1
+    
+    try: 
+        sample_rate, samples = wav.read(path)
+    except:
+        print("Error reading file")
+    
+
+    return [samples, sample_rate]
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def parse_arguments():
     return args
 
 def main(args):
-    audio.hello_world()
+    audio.get_samples_and_sample_rate(args.input)
 
 if __name__ == "__main__":
     args = parse_arguments()
