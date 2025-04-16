a8bf93855d9ee49e7361a3e2e6fa5670fc09e27a
GRAHAM EISELE 347585 <graham.w.eisele@gmail.com>
2025-04-16 20:59:57 +0200

    feat: added user delete request method

--- server.py
+++ server.py
@@ -14,6 +14,11 @@ def homepage():
 
 @app.route("/uploadvideo", methods=['POST'])
 def upload_video():
+
+    if(len(os.listdir("video"))) > 0:
+        flash('Video Already Uploaded')
+        return redirect('/')
+
     if 'file' not in request.files:
         flash('No file part in the request')
         return redirect('/')
@@ -28,10 +33,23 @@ def upload_video():
         flash('File Not MP4 Format')
         return redirect('/')
     
-    file.save("video//" + file.filename)
+    file.save("video//video.mp4")
     
     print(file.filename)
     return redirect('/')
+
+@app.route("/deletevideo", methods=['POST'])
+def delete_video():
+
+    print("test")
+
+    if os.path.exists("video//video.mp4"):
+        os.remove("video//video.mp4")
+        return redirect('/')
+    
+    flash('No file found to delete')
+    return redirect('/')
+
 
 def start_server():
     app.run("0.0.0.0", port=80, debug=True)
